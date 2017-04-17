03ca0a74c77e611957eb8c6398e29b2caf5bdcd7
Gustavo RPS <email+c9.io@gustavorps.net>
2017-04-17 19:37:55 +0000

    bug fix and improved CleanHTML

--- ze/processors/html.py
+++ ze/processors/html.py
@@ -8,51 +8,51 @@ logger = logging.getLogger(__name__)
 
 class CleanHTML(object):
 
-    def __call__(self, value, context=None):
-        soup = BeautifulSoup(value, 'html.parser')
+    def __call__(self, value, context={}):
+        html = BeautifulSoup(value, 'html.parser')
         
         # veja
-        for e in soup.select('.featured-image'):
-            fg = soup.new_tag('figure')
-            fg.append(soup.new_tag('img', src=e.select('img')[0]['data-src']))
-            fc = soup.new_tag('figcaption')
+        for e in html.select('.featured-image'):
+            fg = html.new_tag('figure')
+            fg.append(html.new_tag('img', src=e.select('img')[0]['data-src']))
+            fc = html.new_tag('figcaption')
             fc.string = e.select('p')[0].string
             fg.append(fc)
             
             e.replace_with(fg)
             
-        for e in soup.select('p span iframe[src*="https://www.youtube.com/embed"]'):    
+        for e in html.select('p span iframe[src*="https://www.youtube.com/embed"]'):    
             video_id = e['data-lazy-src'].split('/')[4]
-            fm = soup.new_tag('iframe', src='https://www.youtube.com/embed/%s?rel=0' % video_id,
+            fm = html.new_tag('iframe', src='https://www.youtube.com/embed/%s?rel=0' % video_id,
                 width='1280', height='720', frameborder='0', allowfullscreen='true')
         
         # g1
-        for e in soup.select('[data-block-type="backstage-photo"]'):
-            fg = soup.new_tag('figure')
-            fg.append(soup.new_tag('img', src=e.select('img.content-media__image')[0]['data-src']))
-            fc = soup.new_tag('figcaption')
+        for e in html.select('[data-block-type="backstage-photo"]'):
+            fg = html.new_tag('figure')
+            fg.append(html.new_tag('img', src=e.select('img.content-media__image')[0]['data-src']))
+            fc = html.new_tag('figcaption')
             fc.string = e.select('img.content-media__image')[0]['alt']
             fg.append(fc)
             
             e.replace_with(fg)
         
-        for e in soup.select('[data-block-type="backstage-video"]'):
+        for e in html.select('[data-block-type="backstage-video"]'):
             video_id = e.select('[data-video-id]')[0]['data-video-id']
-            fg = soup.new_tag('figure')
-            fg.append(soup.new_tag('img', src='https://s02.video.glbimg.com/x720/%s.jpg' % video_id))
-            fc = soup.new_tag('figcaption')
+            fg = html.new_tag('figure')
+            fg.append(html.new_tag('img', src='https://s02.video.glbimg.com/x720/%s.jpg' % video_id))
+            fc = html.new_tag('figcaption')
             fc.string = e.select('[itemprop="caption"]')[0]['content']
             fg.append(fc)
-            a = soup.new_tag('a', href='https://globoplay.globo.com/v/%s/' % video_id)
+            a = html.new_tag('a', href='https://globoplay.globo.com/v/%s/' % video_id)
             a.append(fg)
             
             e.replace_with(a)
         
         # cartacapital
-        for e in soup.select('p iframe[data-lazy-src*="https://www.youtube.com/embed"]'):
+        for e in html.select('p iframe[data-lazy-src*="https://www.youtube.com/embed"]'):
             # TODO: Use Regex?
             video_id = e['data-lazy-src'].split('/')[4]
-            fm = soup.new_tag('iframe', src='https://www.youtube.com/embed/%s?rel=0' % video_id,
+            fm = html.new_tag('iframe', src='https://www.youtube.com/embed/%s?rel=0' % video_id,
                 width='1280', height='720', frameborder='0', allowfullscreen='true')
             
             e.parent.replace_with(fm)
@@ -64,12 +64,12 @@ class CleanHTML(object):
             '.content', 
             '.content-text', 
             '.article-content', 
-            '.content-intertitle,', 
+            '.content-intertitle', 
             '.td-post-content', 
             '.td-post-featured-image', 
             '[itemprop="articleBody"]', 
         ] if not context.get('el_to_uwrap') else context.get('el_to_uwrap')
-        [e.unwrap() for e in soup.select(','.join(el_to_uwrap))]
+        [e.unwrap() for e in html.select(','.join(el_to_uwrap))]
             
         el_to_decompose = {
             'geral': [
@@ -78,6 +78,7 @@ class CleanHTML(object):
                 '#liveblog-container', 
                 '.content-head', 
                 '.content-share-bar', 
+                '[data-block-type="related-articles"]', 
                 '.mc-side-item__container', 
                 '.mc-show-later', 
                 '.content-share-bar', 
@@ -88,11 +89,11 @@ class CleanHTML(object):
             ],
             'empty': ['p', 'div',]
         } if not context.get('el_to_decompose') else context.get('el_to_decompose')
-        [e.decompose() for e in soup.select(','.join(el_to_decompose['geral']))]
-        [e.decompose() for e in soup.select(','.join(el_to_decompose['empty']))]
+        [t.decompose() for t in html.select(','.join(el_to_decompose['geral']))]
+        [t.decompose() for t in html.select(','.join(el_to_decompose['empty'])) if t.is_empty_element]
         
         # TODO: B4S bug
-        [e.previous_element.decompose() for e in soup.select('p + br + p')]
+        [e.previous_element.decompose() for e in html.select('p + br + p')]
         
         attrs_to_remove = [
             'alt', 
@@ -111,15 +112,18 @@ class CleanHTML(object):
             'cellspacing', 
             'valign', 
         ] if not context.get('attrs_to_remove') else context.get('attrs_to_remove')
-        for e in soup.select('*'):
-            [e.pop(a, None) for a in attrs_to_remove]
+        for t in html.find_all():
+            for a in attrs_to_remove:
+                if t.has_attr(a): del t[a] 
+                else: pass
+                
         
-        for e in soup.select('td p s'):
+        for e in html.select('td p s'):
             e.parent.parent.string = e.string
         
-        for comments in soup.findAll(text=lambda text:isinstance(text, Comment)):
+        for comments in html.findAll(text=lambda text:isinstance(text, Comment)):
             comments.extract()
         
-        value = soup.prettify()
+        value = html.prettify()
         # print(value)
         return value
