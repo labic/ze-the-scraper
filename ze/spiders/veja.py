1ccda1d71760fcd287733ee9dd2c4f6607aa05fd
ligia.venturott <ligia.venturott@gmail.com>
2017-04-19 16:47:57 -0300

    Modifiquei em, gestaoescola, valor economico

--- ze/spiders/veja.py
+++ ze/spiders/veja.py
@@ -3,43 +3,44 @@
 from ze.spiders import ZeSpider
 
 class VejaSpider(ZeSpider):
-    
+
     name = 'veja'
     allowed_domains = ['veja.abril.com.br']
     parses = [{
         "ze.items.creativework.ArticleItem": {
-            "fields": { 
-                "name": [ 
-                    "[itemprop=name]::text", 
-                    ".article-title::text" 
-                ], 
-                "image": [ 
-                    "[itemprop=image]::attr(content)", 
-                    "[property=og:image]::attr(content)" 
-                ], 
-                "description": [ 
-                    "[itemprop=description]::text", 
-                    ".article-subtitle::text",  
-                ], 
+            "fields": {
+                "name": [
+                    "[itemprop=name]::text",
+                    ".article-title::text"
+                ],
+                "image": [
+                    "[itemprop=image]::attr(content)",
+                    "[property=og:image]::attr(content)"
+                ],
+                "description": [
+                    "[itemprop=description]::text",
+                    ".article-subtitle::text",
+                ],
                 "author": [
-                    "[itemprop=author]::text", 
-                    ".article-author span strong::text", 
-                ], 
+                    "[itemprop=author]::text",
+                    ".article-author span strong::text",
+                ],
                 "datePublished": [
                     "[itemprop=datePublished]::text",
                     ".article-date span::text"
-                ], 
+                    ".entry-date::text"
+                ],
                 "dateModified": [
-                    "[itemprop=dateModified]::text", 
-                    ".article-date span::text" 
-                ], 
+                    "[itemprop=dateModified]::text",
+                    ".article-date span::text"
+                ],
                 "articleBody": [
                     "[itemprop=articleBody]",
-                    ".article-content" 
-                ], 
+                    ".article-content"
+                ],
                 "keywords": [
-                    "[itemprop=keywords]::text", 
-                    ".article-tags a::text" 
+                    "[itemprop=keywords]::text",
+                    ".article-tags a::text"
                 ]
             }
         }
