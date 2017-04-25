d9ac33a6dafa42f60aed6231a4f87e4d5731fcaa
Gustavo RPS <email+c9.io@gustavorps.net>
2017-04-25 20:33:16 +0000

    created function to diff strings

--- ze/utils/__init__.py
+++ ze/utils/__init__.py
@@ -1,6 +1,13 @@
+import sys
 import importlib
+import difflib
+from pprint import pprint
 
 def import_class(class_full_path):
     module_name, class_name = class_full_path.rsplit('.', 1)
     module = importlib.import_module(module_name)
     return getattr(module, class_name)
+
+def diff_str(str1='', str2=''):
+    splitlines = lambda s: s.splitlines(keepends=True)
+    return list(difflib.Differ().compare(splitlines(str1), splitlines(str2)))
