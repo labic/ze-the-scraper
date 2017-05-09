d809551407fddfe7f5b39ec3c928f3a06e7bb4a8
Gustavo RPS <email+c9.io@gustavorps.net>
2017-05-09 19:34:57 +0000

    bug fix: change name of setting

--- ze/extensions/google/__init__.py
+++ ze/extensions/google/__init__.py
@@ -14,7 +14,7 @@ class GoogleCloud(object):
         self.google_cloud_enabled = settings.getbool('GOOGLE_CLOUD_ENABLED')
         
         if self.google_cloud_enabled:
-            credentials_json = settings.get('GOOGLE_APPLICATION_CREDENTIALS_JSON')
+            credentials_json = settings.get('GOOGLE_CLOUD_APPLICATION_CREDENTIALS_JSON')
             if credentials_json:
                 if not os.path.isfile(self.credentials_json_path):
                     with open(self.credentials_json_path, 'w') as outfile:
@@ -23,7 +23,7 @@ class GoogleCloud(object):
                 os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = self.credentials_json_path
                 logger.info('Google Cloud extensions inited with success')
             else:
-                logger.error('GOOGLE_APPLICATION_CREDENTIALS_JSON not is set in settings')
+                logger.error('GOOGLE_CLOUD_APPLICATION_CREDENTIALS_JSON not is set in settings')
                 settings.set('GOOGLE_CLOUD_ENABLED', False)
         else:
             logger.info('GOOGLE_CLOUD_ENABLED is False')
