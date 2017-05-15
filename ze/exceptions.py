38b49d85f42f20b4c4d324a16f10154131d3e608
Gustavo RPS <email+c9.io@gustavorps.net>
2017-05-15 15:16:12 -0300

    added a new exeption

--- ze/exceptions.py
+++ ze/exceptions.py
@@ -4,3 +4,7 @@ from scrapy.exceptions import DropItem
 class EmptyFields(DropItem):
     """Drop item from the item pipeline with empty fields"""
     pass
+
+class DesiredTermsMissing(DropItem):
+    """Drop item from the item pipeline with empty fields"""
+    pass
