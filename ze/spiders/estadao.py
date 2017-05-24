d539b8e3307354f4b68175d97fbed8ed330d3aac
ligia.venturott <ligia.venturott@gmail.com>
2017-05-24 17:43:42 -0300

    merged estadao

--- ze/spiders/estadao.py
+++ ze/spiders/estadao.py
@@ -8,7 +8,6 @@ class EstadaoSpider(ZeSpider):
     allowed_domains = ['estadao.com.br']
     parses = [{
         "ze.items.creativework.ArticleItem": {
-<<<<<<< 6528394ffb2b78739a5e3621d0a42298e2c4be17
             "fields": { 
                 "name": [ 
                     "[itemprop=name]::text", 
@@ -27,7 +26,6 @@ class EstadaoSpider(ZeSpider):
                     ".n--noticia__subtitle::text", 
                     "article p::text", 
                 ], 
-||||||| merged common ancestors
             "fields": { 
                 "name": [ 
                     "[itemprop=name]::text", 
@@ -41,7 +39,6 @@ class EstadaoSpider(ZeSpider):
                     "[itemprop=description]::text", 
                     ".linha-fina::text" 
                 ], 
-=======
             "fields": {
                 "name": [
                     "[itemprop=name]::text",
@@ -55,22 +52,17 @@ class EstadaoSpider(ZeSpider):
                     "[itemprop=description]::text",
                     ".linha-fina::text"
                 ],
->>>>>>> revisao de codigos
                 "author": [
-<<<<<<< 6528394ffb2b78739a5e3621d0a42298e2c4be17
                     "[itemprop=author]::text", 
                     ".autor::text", 
                     ".author::text", 
                 ], 
-||||||| merged common ancestors
                     "[itemprop=author]::text", 
                     ".autor::text"
                 ], 
-=======
                     "[itemprop=author]::text",
                     ".autor::text"
                 ],
->>>>>>> revisao de codigos
                 "datePublished": [
                     "[itemprop=datePublished]::text",
                     ".data::text", 
@@ -80,33 +72,23 @@ class EstadaoSpider(ZeSpider):
                 ],
                 "articleBody": [
                     "[itemprop=articleBody]",
-<<<<<<< 6528394ffb2b78739a5e3621d0a42298e2c4be17
                     ".main-news .content",
                     ".conteudo-materia",
                     ".content", 
-                ], 
-||||||| merged common ancestors
                     ".main-news .content",
                     ".conteudo-materia",
-                ], 
-=======
                     ".entry"
                 ],
->>>>>>> revisao de codigos
                 "keywords": [
-<<<<<<< 6528394ffb2b78739a5e3621d0a42298e2c4be17
                     "[itemprop=keywords] a::text", 
                     ".tags a::text", 
                     ".tags a span::text", 
-||||||| merged common ancestors
                     "[itemprop=keywords] a::text", 
                     ".tags a::text", 
-                    ".tags a span::text" 
-=======
+                    ".tags a span::text",  
                     "[itemprop=keywords] a::text",
                     ".tags a::text",
                     ".tags a span::text"
->>>>>>> revisao de codigos
                 ]
             }
         }
