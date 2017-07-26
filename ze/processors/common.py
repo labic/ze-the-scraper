41b576dee05d1ef48101d11043dc1aa2536babf5
ligiaiv <ligia.venturott@gmail.com>
2017-07-26 10:50:20 -0300

    dateParser

--- ze/processors/common.py
+++ ze/processors/common.py
@@ -3,6 +3,18 @@
 import dateparser
 import logging; logger = logging.getLogger(__name__)
 
+tabela_meses = {'janeiro':1,
+                'fevereiro':2,
+                'março':3,
+                'abril':4,
+                'maio':5,
+                'junho':6,
+                'julho':7,
+                'agosto':8,
+                'setembro':9,
+                'outubro':10,
+                'novembro':11,
+                'dezembro':12}
 
 class CleanString(object):
 
@@ -15,22 +27,31 @@ class ParseDate(object):
     def __call__(self, value, loader_context):
         spider_name = loader_context.get('spider_name')
 
+        print('------------------------------------------------------------------------------------\n'+str(value)+'\n--------------------------------------------------------------------')
+
         if spider_name == 'r7':
             value=value.split('(')[1].split(')')[0]
             print ('value  ',value)
 
+        if spider_name == 'correiobraziliense':
+            return dateparser.parse(value, settings={'TIMEZONE': '+0300','DATE_ORDER': 'DMY'})
+
+        # if spider_name =='gestaoescolar':
+        #     # value = value.strip(' de ')split('')
+        #     return(dateparser.parse(value, settings={'TIMEZONE': '+0300'}))
 
 
-        value = value.replace('Atualizado:', '') \
-                     .replace('Atualizado', '') \
-                     .replace(' | ', ' ') \
-                     .replace('h', ':') \
-                     .replace('h ', ':') \
-                     .replace(', ', ' ') \
-                     .replace('  ', ' ') \
-                     .strip()
+        # value = value.replace('Atualizado:', '') \
+        #              .replace('Atualizado', '') \
+        #              .replace(' | ', ' ') \
+        #              .replace('h', ':') \
+        #              .replace('h ', ':') \
+        #              .replace(', ', ' ') \
+        #              .replace('  ', ' ') \
+        #              .strip()
 
         try:
+            print(dateparser.parse(value, settings={'TIMEZONE': '+0300'}))
             return dateparser.parse(value, settings={'TIMEZONE': '+0300'})
         except Exception as e:
             logger.warning('Date not processed: %s' % value)
