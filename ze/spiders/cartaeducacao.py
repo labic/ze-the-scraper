4d3769669ad2ccc32a8240a93c4e4880e85cb7b4
Gustavo RPS <email+c9.io@gustavorps.net>
2017-08-26 20:53:01 -0300

    changed items references data structure for PortalUAISpider and CartaEducacaoSpider

--- ze/spiders/cartaeducacao.py
+++ ze/spiders/cartaeducacao.py
@@ -1,56 +1,87 @@
 # -*- coding: utf-8 -*-
+from . import ZeSpider
 
-from ze.spiders import ZeSpider
 
 class CartaEducacaoSpider(ZeSpider):
 
     name = 'cartaeducacao'
     allowed_domains = ['cartaeducacao.com.br']
-    parses = [{
-        "ze.items.creativework.ArticleItem": {
-            "fields": {
-                "name": [
-                    "meta[property='og:title']::attr(content)",
-                    "meta[name=title]::attr(content)",
-                    "[itemprop=name]::text",
-                    ".documentFirstHeading::text"
-                ],
-                "image": [
-                    'meta[property="og:image"]::attr(content)',
-                    "[itemprop=image]::attr(content)",
-                    "[property='og:image']::attr(content)"
-                ],
-                "description": [
-                    "meta[property='og:description']::attr(content)",
-                    "meta[name=description]::attr(content)",
-                    "[itemprop=description]::text",
-                    ".documentDescription::text"
-                ],
-                "author": [
-                    "[itemprop=author]::text",
-                    ".documentAuthor a::text",
-                    ".documentAuthor::text",
-                    ".td-post-author-name::text"
-                ],
-                "datePublished": [
-                    "[itemprop=datePublished]::attr(content)",
-                    ".documentPublished::text"
-                ],
-                "dateModified": [
-                    "[itemprop=dateModified]::text",
-                    '.documentModified::text'
-                ],
-                "articleBody": [
-                    "[itemprop=articleBody]",
-                    "#content-core",
-                    ".td-post-content",
-                ],
-                "keywords": [
-                    "[itemprop=keywords]::text",
-                    "[property='rnews:keywords']::text",
-                    "[rel='tag']::text",
-                    "#category .link-category::text"
-                ]
+    items_refs = [{
+        "item": "ze.items.creativework.ArticleItem",
+        "fields": {
+            "name": {
+                "selectors": {
+                    "css": [
+                        "meta[property='og:title']::attr(content)",
+                        "meta[name=title]::attr(content)",
+                        "[itemprop=name]::text",
+                        ".documentFirstHeading::text"
+                    ]
+                }
+            },
+            "image": {
+                "selectors": {
+                    "css": [
+                        'meta[property="og:image"]::attr(content)',
+                        "[itemprop=image]::attr(content)",
+                        "[property='og:image']::attr(content)"
+                    ]
+                }
+            },
+            "description": {
+                "selectors": {
+                    "css": [
+                        "meta[property='og:description']::attr(content)",
+                        "meta[name=description]::attr(content)",
+                        "[itemprop=description]::text",
+                        ".documentDescription::text"
+                    ]
+                }
+            },
+            "author": {
+                "selectors": {
+                    "css": [
+                        "[itemprop=author]::text",
+                        ".documentAuthor a::text",
+                        ".documentAuthor::text",
+                        ".td-post-author-name::text"
+                    ]
+                }
+            },
+            "datePublished": {
+                "selectors": {
+                    "css": [
+                        "[itemprop=datePublished]::attr(content)",
+                        ".documentPublished::text"
+                    ]
+                }
+            },
+            "dateModified": {
+                "selectors": {
+                    "css": [
+                        "[itemprop=dateModified]::text",
+                        '.documentModified::text'
+                    ]
+                }
+            },
+            "articleBody": {
+                "selectors": {
+                    "css": [
+                        "[itemprop=articleBody]",
+                        "#content-core",
+                        ".td-post-content"
+                    ]
+                }
+            },
+            "keywords": {
+                "selectors": {
+                    "css": [
+                        "[itemprop=keywords]::text",
+                        "[property='rnews:keywords']::text",
+                        "[rel='tag']::text",
+                        "#category .link-category::text"
+                    ]
+                }
             }
         }
     }]
