6bc54ae7637c12eb764d02e68fb3a8d31372aed9
Unknown <ligia.venturott@gmail.com>
2017-08-28 12:15:40 -0300

    new spiders
    sbt isn't ready

---
+++ ze/spiders/brasilescola.py
@@ -0,0 +1,82 @@
+# -*- coding: utf-8 -*-
+from . import ZeSpider
+
+
+class BrasilEscolaSpider(ZeSpider):
+
+    name = 'brasilescola'
+    allowed_domains = ['brasilescola.uol.com.br']
+    items_refs = [{
+        "item": "ze.items.creativework.ArticleItem",
+        "fields": {
+            "name": {
+                "selectors": {
+                    "css": [
+                        "meta[property='og:title']::attr(content)",
+                        "meta[name=title]::attr(content)",
+                        '[itemprop=headline]::text',
+                        '.materia h1::text'
+                    ]
+                }
+            },
+            "image": {
+                "selectors": {
+                    "css": [
+                        'meta[property="og:image"]::attr(content)',
+                        '[itemprop="image"] img::attr(src)'
+                    ]
+                }
+            },
+            "description": {
+                "selectors": {
+                    "css": [
+                        "meta[property='og:description']::attr(content)",
+                        "meta[name=description]::attr(content)",
+                        '[itemprop=description]::attr(content)',
+                        '[itemprop=description]::text',
+                        '.resumo h2::text'
+                    ]
+                }
+            },
+            "author": {
+                "selectors": {
+                    "css": [
+                        '[itemprop=author]::text',
+                        '.autor-nome::text'
+                    ]
+                }
+            },
+            "datePublished": {
+                "selectors": {
+                    "css": [
+                        '[itemprop=datePublished]::attr(content)',
+                        '.data::text'
+                    ]
+                }
+            },
+            "dateModified": {
+                "selectors": {
+                    "css": [
+                        '[itemprop=dateModified]::attr(content)'
+                    ]
+                }
+            },
+            "articleBody": {
+                "selectors": {
+                    "css": [
+                        '[itemprop=articleBody]',
+                        '.conteudo-materia'#ta dando algo errado aqui. não consigo pegar todos os <p>s
+                    ]
+                }
+            },
+            "keywords": {
+                "selectors": {
+                    "css": [
+                        '[itemprop=keywords] a::text',
+                        '[rel=tag]::text',
+                        '[onclick*=montaURL]::text'
+                    ]
+                }
+            }
+        }
+    }]
---
+++ ze/spiders/sbt.py
@@ -0,0 +1,89 @@
+# -*- coding: utf-8 -*-
+from . import ZeSpider
+
+
+class SBTSpider(ZeSpider):
+
+    name = 'sbt'
+    allowed_domains = ['sbt.com.br']
+    items_refs = [{
+        "item": "ze.items.creativework.ArticleItem",
+        "fields": {
+            "name": {
+                "selectors": {
+                    "css": [
+                        "meta[property='og:title']::attr(content)",
+                        "meta[name=title]::attr(content)",
+                        '[itemprop=headline]::text',
+                        '.title-post::text',
+                        '.entry-title::text'
+                    ]
+                }
+            },
+            "image": {
+                "selectors": {
+                    "css": [
+                        "meta[property='og:description']::attr(content)",
+                        "meta[name=description]::attr(content)",
+                        'meta[property="og:image"]::attr(content)',
+                        '[itemprop="image"]::attr(src)',
+                        '.lazy::attr("data-lazy-src")'
+                    ]
+                }
+            },
+            "description": {
+                "selectors": {
+                    "css": [
+                        '[itemprop=description]::attr(content)',
+                        '[itemprop=description]::text',
+                        '.entry-content h2::text',
+                        '.linha-fina::text',
+                        '.entry-content blockquote p::text'
+                    ]
+                }
+            },
+            "author": {
+                "selectors": {
+                    "css": [
+                        '[itemprop=author]::text',
+                        '.author a::text',
+                        '[href*="mailto"]::text',
+                        '.news-data-pub__author span::text'
+                    ]
+                }
+            },
+            "datePublished": {
+                "selectors": {
+                    "css": [
+                        '[itemprop=datePublished]::attr(content)',
+                        '.entry-date::text',
+                        'h1 .titleNoticeSocial::text'
+                    ]
+                }
+            },
+            "dateModified": {
+                "selectors": {
+                    "css": [
+                        '[itemprop=dateModified]::attr(content)'
+                    ]
+                }
+            },
+            "articleBody": {
+                "selectors": {
+                    "css": [
+                        '[itemprop=articleBody]',
+                        '.entry-content'
+                    ]
+                }
+            },
+            "keywords": {
+                "selectors": {
+                    "css": [
+                        '[itemprop=keywords] a::text',
+                        '[rel=tag]::text',
+                        '[onclick*=montaURL]::text'
+                    ]
+                }
+            },
+        }
+    }]
