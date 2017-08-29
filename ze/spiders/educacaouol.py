12192137951cff710fb5415463e75956ca7128db
Unknown <ligia.venturott@gmail.com>
2017-08-29 18:06:28 -0300

    New spiders
    educacaouol, mundoeducacao, sejabixo, universia

---
+++ ze/spiders/educacaouol.py
@@ -0,0 +1,84 @@
+# -*- coding: utf-8 -*-
+from . import ZeSpider
+
+
+class EducacaoUolSpider(ZeSpider):
+
+    name = 'educacaouol'
+    allowed_domains = ['educacao.uol.com.br']
+    items_refs = [{
+        "item": "ze.items.creativework.ArticleItem",
+        "fields": {
+            "name": {
+                "selectors": {
+                    "css": [
+                        "meta[property='og:title']::attr(content)",
+                        "meta[name=title]::attr(content)",
+                        '[itemprop=headline]::text',
+                        '.materia h1::text'
+                    ]
+                }
+            },
+            "image": {
+                "selectors": {
+                    "css": [
+                        'meta[property="og:image"]::attr(content)',
+                        '[itemprop="image"] img::attr(src)'
+                    ]
+                }
+            },
+            "description": {
+                "selectors": {
+                    "css": [
+                        "meta[property='og:description']::attr(content)",
+                        "meta[name=description]::attr(content)",
+                        '[itemprop=description]::attr(content)',
+                        '[itemprop=description]::text',
+                        '.resumo h2::text'
+                    ]
+                }
+            },
+            "author": {
+                "selectors": {
+                    "css": [
+                        '[itemprop=author]::text',
+                        '.autor-nome::text',
+                        '.info-header p:first-child::text'
+                    ]
+                }
+            },
+            "datePublished": {
+                "selectors": {
+                    "css": [
+                        '[itemprop=datePublished]::attr(content)',
+                        '.data::text'
+                    ]
+                }
+            },
+            "dateModified": {
+                "selectors": {
+                    "css": [
+                        '[itemprop=dateModified]::attr(content)'
+                    ]
+                }
+            },
+            "articleBody": {
+                "selectors": {
+                    "css": [
+                        '[itemprop=articleBody]',
+                        '.conteudo-materia',
+                        '#texto'#ta dando algo errado aqui. não consigo pegar todos os <p>s
+                    ]
+                }
+            },
+            "keywords": {
+                "selectors": {
+                    "css": [
+                        '[itemprop=keywords] a::text',
+                        '[rel=tag]::text',
+                        '[onclick*=montaURL]::text'
+                    ]
+                }
+            }
+        }
+    }]
