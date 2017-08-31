ef97f8f44cadd7a209e61cd9fa0945c6a4d3b59d
Unknown <ligia.venturott@gmail.com>
2017-08-31 17:40:02 -0300

    New Spider
    Jornal do Brasil, as 'jb' spider

---
+++ ze/spiders/jb.py
@@ -0,0 +1,87 @@
+# -*- coding: utf-8 -*-
+from . import ZeSpider
+
+
+class JornaldoBrasilSpider(ZeSpider):
+
+    name = 'jb'
+    allowed_domains = ['jb.com.br']
+    items_refs = [{
+        "item": "ze.items.creativework.ArticleItem",
+        "fields": {
+            "name": {
+                "selectors": {
+                    "css": [
+                        "meta[property='og:title']::attr(content)",
+                        "meta[name=title]::attr(content)",
+                        '[itemprop=headline]::text',
+                        '.materia h1::text'
+                    ]
+                }
+            },
+            "image": {
+                "selectors": {
+                    "css": [
+                        'meta[property="og:image"]::attr(content)',
+                        '[itemprop="image"] img::attr(src)'
+                    ]
+                }
+            },
+            "description": {
+                "selectors": {
+                    "css": [
+                        "meta[property='og:description']::attr(content)",
+                        "meta[name=description]::attr(content)",
+                        '[itemprop=description]::attr(content)',
+                        '[itemprop=description]::text',
+                        '.resumo h2::text'
+                    ]
+                }
+            },
+            "author": {
+                "selectors": {
+                    "css": [
+                        '[itemprop=author]::text',
+                        '.author::text'
+                    ]
+                }
+            },
+            "datePublished": {
+                "selectors": {
+                    "css": [
+                        '[itemprop=datePublished]::attr(content)',
+                        '.data::text',
+                        '#content article time:first-child ::attr(datetime)'
+                    ]
+                }
+            },
+            "dateModified": {
+                "selectors": {
+                    "css": [
+                        '[itemprop=dateModified]::attr(content)',
+                        '#content article time:nth-child(2) ::attr(datetime)'
+
+                    ]
+                }
+            },
+            "articleBody": {
+                "selectors": {
+                    "css": [
+                        '[itemprop=articleBody]',
+                        '.conteudo-materia',
+                        '#content #HOTWordsTxt'
+                    ]
+                }
+            },
+            "keywords": {
+                "selectors": {
+                    "css": [
+                        '[itemprop=keywords] a::text',
+                        '[rel=tag]::text',
+                        '[onclick*=montaURL]::text',
+                        '.tags span::text'
+                    ]
+                }
+            }
+        }
+    }]
