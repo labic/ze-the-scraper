e8fe030fb016202ae60697a826b75daf1636a6a0
Gustavo RPS <email+c9.io@gustavorps.net>
2017-10-09 15:01:29 -0300

    fixed selectors

--- ze/spiders/sejabixo.py
+++ ze/spiders/sejabixo.py
@@ -22,8 +22,8 @@ class SejaBixoSpider(ZeSpider):
             "image": {
                 "selectors": {
                     "css": [
-                        'meta[property="og:image"]::attr(content)',
-                        '[itemprop="image"] img::attr(src)'
+                        "meta[property='og:image']::attr(content)",
+                        "[itemprop='image'] img::attr(src)"
                     ]
                 }
             },
@@ -32,53 +32,53 @@ class SejaBixoSpider(ZeSpider):
                     "css": [
                         "meta[property='og:description']::attr(content)",
                         "meta[name=description]::attr(content)",
-                        '[itemprop=description]::attr(content)',
-                        '[itemprop=description]::text',
-                        '.resumo h2::text'
+                        "[itemprop=description]::attr(content)",
+                        "[itemprop=description]::text",
+                        ".resumo h2::text"
                     ]
                 }
             },
             "author": {
                 "selectors": {
                     "css": [
-                        '[itemprop=author]::text',
-                        '.autor-nome::text'
+                        "[itemprop=author]::text",
+                        ".autor-nome::text"
                     ]
                 }
             },
             "datePublished": {
                 "selectors": {
                     "css": [
-                        '[itemprop=datePublished]::attr(content)',
-                        '.data::text',
-                        'article div[align=center] strong::text',
-                        'article i strong::text',
-                        '"#content i strong"::text'
+                        "[itemprop=datePublished]::attr(content)",
+                        ".data::text",
+                        "article div[align=center] strong::text",
+                        "article i strong::text",
+                        "#content i strong::text"
                     ]
                 }
             },
             "dateModified": {
                 "selectors": {
                     "css": [
-                        '[itemprop=dateModified]::attr(content)'
+                        "[itemprop=dateModified]::attr(content)"
                     ]
                 }
             },
             "articleBody": {
                 "selectors": {
                     "css": [
-                        '[itemprop=articleBody]',
-                        '.conteudo-materia',
-                        'article'
+                        "[itemprop=articleBody]",
+                        ".conteudo-materia",
+                        "article"
                     ]
                 }
             },
             "keywords": {
                 "selectors": {
                     "css": [
-                        '[itemprop=keywords] a::text',
-                        '[rel=tag]::text',
-                        '[onclick*=montaURL]::text'
+                        "[itemprop=keywords] a::text",
+                        "[rel=tag]::text",
+                        "[onclick*=montaURL]::text"
                     ]
                 }
             }
